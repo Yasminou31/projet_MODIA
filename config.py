f331492768e864ab22ba8d9a4aafdb2c523f6924
mountassir-el <elmoustaaidmountassir@gmail.com>
2023-06-19 00:00:55 +0200

    feat: compute the ratings count and use it to compute the popularity

---
+++ config.py
@@ -0,0 +1,5 @@
+# Dataset configurations
+raw_interactions_path: str = "./data/RAW_interactions.csv"
+raw_recipes_path: str = "./data/RAW_recipes.csv"
+interactions_train_path: str = "./data/interactions_train.csv"
+interactions_test_path: str = "./data/interactions_test.csv"
